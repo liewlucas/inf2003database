1d2ad21d2dd36498390cdd612aa68b4b0164dd3a
keyi10 <leekeeyi10@gmail.com>
2023-11-25 01:51:25 +0800

    postDealer Update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import HomePage from "./pages/Home"
 import Login from "./pages/Login"
 import Posts from "./pages/Posts"
 import Footer from "./Components/Footer";
+import AddPost from "./pages/PostsDealerAdd"
+import PostsDealer from "./pages/PostsDealer"
+
 
 
 function App() {
@@ -16,6 +19,8 @@ function App() {
           <Route path="/" exact Component={HomePage}/> 
           <Route path="/Login" exact Component={Login}/> 
           <Route path="/Posts" exact Component={Posts}/> 
+          <Route path="/PostsDealerAdd" exact Component={AddPost}/> 
+          <Route path="/PostsDealer" exact Component={PostsDealer} />
         </Routes>
         <Footer/>
       </Router>
