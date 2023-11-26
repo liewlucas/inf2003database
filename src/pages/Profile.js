a816390e3f5496e801e8c32c7d76cd65c19ba0f1
lucasliew <lucasliew88@gmail.com>
2023-11-26 21:53:21 +0800

    update profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,41 +2,52 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../helpers/AuthContext';
+import jwt from 'jsonwebtoken';
 
 const Profile = () => {
   const [userDetails, setUserDetails] = useState({});
   const [loading, setLoading] = useState(true);
   const { token, logout } = useAuth();
   const navigate = useNavigate();
+  const [userID, setUserID] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(!!token);
 
   useEffect(() => {
     const fetchUserDetails = async () => {
       try {
-        if (!token) {
-          throw new Error('User not authenticated');
+        const decodedToken = jwt.decode(token);
+        if(token)
+        {if (decodedToken) {
+          setUserID(decodedToken.userId);
         }
-
-        const response = await axios.get('http://localhost:3001/api/getuserdetails', {
+  
+        const response = await axios.get(`http://localhost:3001/api/getuserdetails`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-
-        setUserDetails(response.data.userDetails);
+  
+        setUserDetails(response.data.userDetails.userDetails); // Updated line
         setLoading(false);
+      }
+        
       } catch (error) {
         console.error('Error fetching user details:', error);
         setLoading(false);
-
-        // // If unauthorized, redirect to the login page
+      
+        // Log more information about the error
+        console.error('AxiosError details:', error.toJSON());
+      
+        // Handle unauthorized or other errors
         // if (error.response && error.response.status === 401) {
+        //   logout();
         //   navigate('/Login');
         // }
-      }
-    };
-
+    }
+  }
+  
     fetchUserDetails();
-  }, [token, navigate]);
+  }, [token, navigate, logout]);
 
   const handleLogout = () => {
     logout();
